f9cc125150797e3598bd401932e54dabd9a572cc
JNing <hok2016jn@gmail.com>
2017-06-01 10:03:09 +0800

    Delete utils.py

--- FCN/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib as mpl
-import matplotlib.cm
-
-def color_image(image, num_classes=20):
-
-    norm = mpl.colors.Normalize(vmin=0., vmax=num_classes)
-    mycm = mpl.cm.get_cmap('Set1')
-    return mycm(norm(image))
