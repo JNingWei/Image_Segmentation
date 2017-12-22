9f9399028e23340f7a88fd81b0e2495a52787b8e
Petrichor <hok2016jn@gmail.com>
2017-12-22 17:19:01 +0800

    Update utils.py

--- FCN/src/utils.py
+++ FCN/src/utils.py
@@ -1,9 +1,16 @@
+# coding=utf-8
+
+"""
+Image-Segmentation
+Expansion package.
+__author__ = 'JNingWei'
+"""
+
 import numpy as np
 import matplotlib as mpl
 import matplotlib.cm
 
 def color_image(image, num_classes=20):
-
     norm = mpl.colors.Normalize(vmin=0., vmax=num_classes)
     mycm = mpl.cm.get_cmap('Set1')
     return mycm(norm(image))
